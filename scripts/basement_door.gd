cc9d043638df9fa1a10c243d5b6c5ee419d63f73
Hasan <hasanahmedhaf@gmail.com>
2025-02-02 15:19:58 -0600

    Add locking system to basement doors

--- scripts/basement_door.gd
+++ scripts/basement_door.gd
@@ -2,12 +2,14 @@ extends Interactable
 
 @export var destination: Marker3D
 @export var level: Global.Levels
+@export var locked: bool = false
+@export var locked_text: String 
 
 var interact_text_override: String
 
 func _ready() -> void:
 	interact_text_override = "{0} [{1}]".format([interact_text, Global.interact_bind])
-	interact_text = interact_text_override
+	interact_text = update_interact_text(locked)
 
 func interact(_player: CharacterBody3D) -> void:
 	super(_player)
@@ -15,3 +17,9 @@ func interact(_player: CharacterBody3D) -> void:
 	SignalBus.changed_level.emit(level)
 	_player.global_position = destination.global_position
 	_player.global_rotation = destination.global_rotation
+
+func update_interact_text(locked: bool) -> String:
+	if locked:
+		return locked_text
+	else:
+		return interact_text_override
