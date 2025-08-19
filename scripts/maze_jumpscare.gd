10f2a7c677e407524c6e1c145b085b7b851c9558
Hasan <hqazi6@gatech.edu>
2025-08-18 19:09:03 -0500

    Add grass

--- scripts/maze_jumpscare.gd
+++ scripts/maze_jumpscare.gd
@@ -3,11 +3,13 @@ extends Node3D
 @export var player: CharacterBody3D
 
 @onready var player_dest: Marker3D = %PlayerDest
+@onready var stalker_model: Node3D = %stalker4
 
 @onready var items := get_tree().get_nodes_in_group("item")
 
 func _ready() -> void:	
 	SignalBus.player_respawn.connect(player_respawn)
+	stalker_model.visible = false
 
 func player_respawn() -> void:
 	items = get_tree().get_nodes_in_group("item")
@@ -18,7 +20,6 @@ func player_respawn() -> void:
 			node.visible = false
 	
 	Global.player_immobile = true
-	player.respawn_button.visible = true
 	
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	
@@ -26,3 +27,7 @@ func player_respawn() -> void:
 	player.global_rotation = player_dest.global_rotation
 	player.head.global_rotation = player_dest.global_rotation
 	#player.player_cam.global_rotation = player_dest.global_rotation
+
+	await get_tree().create_timer(2.0).timeout
+	stalker_model.visible = true
+	player.respawn_button.visible = true
